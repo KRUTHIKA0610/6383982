008d38c14d9abb1185b5d7f48ab80ee666a0c306
Kruthika V <kruthikatut2004@gmail.com>
2025-06-23 01:28:01 +0530

    Cleaned repo to keep only Code and Output folders

--- Week1_DesignPattern/Code/JavaPhase2/src/example/Demo.java
+++
@@ -1,9 +0,0 @@
-package example;
-
-public interface Demo{
-public static void main(String[] args) {     
-    final int a = 3;
-        System.out.println(a);
-    }	
-}
-
--- Week1_DesignPattern/Code/JavaPhase2/src/example/Parent1.java
+++
@@ -1,8 +0,0 @@
-package example;
-
-public class Parent1
-{
-public  void m1() {
-	System.out.println("m1");
-}
-}
